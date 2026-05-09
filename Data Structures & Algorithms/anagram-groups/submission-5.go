ba9a1057ce4d7fc08adfd48eff5c07c3d8cfe314
Chiranjit Datta <myself.chiranjit@gmail.com>
2026-05-10 00:36:05 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.go
@@ -0,0 +1,22 @@
+func groupAnagrams(strs []string) [][]string {
+    if len(strs) <= 1 {
+        return [][]string{strs}
+    }
+
+    groupMap := make(map[[26]int][]string, 101)
+    for _, str := range strs {
+        var counts [26]int
+        for i:=0;i<len(str);i++ {
+            counts[str[i]-'a']++
+        }
+
+        groupMap[counts] = append(groupMap[counts], str)
+    }
+
+    group := make([][]string, 0, len(groupMap))
+    for _, groupStr := range groupMap {
+        group = append(group, groupStr)
+    }
+
+    return group
+}

9f8c56d0225512b7abd2a1b8c008ed2cc4640d20
Chiranjit Datta <myself.chiranjit@gmail.com>
2026-05-09 22:56:19 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.go
@@ -0,0 +1,24 @@
+import "slices"
+
+func groupAnagrams(strs []string) [][]string {
+    // if len(strs) <= 1 {
+    //     return [][]string{strs}
+    // }
+
+    group := make([][]string, 0, 1000)
+    groupMap := make(map[string][]string, 101)
+
+    for _, str := range strs {
+        strRunes := []rune(str)
+        slices.Sort(strRunes)
+        newSortedStr := string(strRunes)
+        groupMap[newSortedStr] = append(groupMap[newSortedStr], str)
+    }
+
+    for _, groupStr := range groupMap {
+        group = append(group, groupStr)
+    }
+
+    return group
+}
+
